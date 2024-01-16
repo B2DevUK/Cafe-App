9399ee39f8c7696d4c828426d9e85441d547b460
B2DevUK <115435623+B2DevUK@users.noreply.github.com>
2024-01-16 18:04:29 +0000

    MainActivity refactoring

--- app/src/main/java/cafe/App/AccountCreationPage.kt
+++ app/src/main/java/cafe/App/AccountCreationPage.kt
@@ -3,9 +3,9 @@ package cafe.App
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AccountCreationPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.account_creation_page)
     }
 }
