07a31012d5bef04c66f540d8634f159bd368b717
B2DevUK <115435623+B2DevUK@users.noreply.github.com>
2024-01-20 16:20:03 +0000

    Removing redundant function & added strings

--- app/src/main/java/cafe/app/ui/menu/MenuFragment.kt
+++ app/src/main/java/cafe/app/ui/menu/MenuFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import cafe.App.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
@@ -22,16 +20,10 @@ class MenuFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val menuViewModel =
-            ViewModelProvider(this).get(MenuViewModel::class.java)
 
         _binding = FragmentMenuBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        menuViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
